f765ec0cc5946b49cddda715bd3fa2272c356e0f
SMAIL ALAHYANE <sm.alahyane@gmail.com>
2024-04-10 16:54:05 +0200

    Quetes num3: afficer les données d'un composant

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
     <PokemonCard />
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,8 +1,24 @@
 function PokemonCard(){
+  const pokemon = pokemonList[0];
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-        alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+      
+      {pokemon.imgSrc ?(
+        <img className="img-card" src={pokemon.imgSrc} alt={pokemon.name}/>
+        ):
+        (<p>???</p>)}
+        <figcaption className="caption">{pokemon.name}</figcaption>
+
     </figure>
-}
+ }
+ const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 export default PokemonCard;
