65f14a04c256b17744b8b52e9843bfb0abe39bd2
SMAIL ALAHYANE <sm.alahyane@gmail.com>
2024-04-13 23:26:13 +0200

    React basics 04 - Passer des props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import './App.css'
 import PokemonCard from './components/PokemonCard'
-function App() {
+
+
+function App (){
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
   return (
     <div>
-    <PokemonCard />
+    <PokemonCard pokemon ={pokemonList[0]}
+    />
     </div>
+     
   )
 }
 
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,24 +1,18 @@
-function PokemonCard(){
-  const pokemon = pokemonList[0];
-    return <figure>
-      
+
+function PokemonCard({pokemon}){
+    return ( 
+    <figure>
+
       {pokemon.imgSrc ?(
         <img className="img-card" src={pokemon.imgSrc} alt={pokemon.name}/>
         ):
         (<p>???</p>)}
         <figcaption className="caption">{pokemon.name}</figcaption>
 
-    </figure>
+    </figure>);
+    
+    
  }
- const pokemonList = [
-    {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+ 
 
 export default PokemonCard;
